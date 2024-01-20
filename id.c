437353c0e7e858ea07bc3593917d0830cc375469
A.S. Pichugina <flow1515@mail.ru>
2024-01-20 23:36:56 +0700

    добавлены комментарии  в исходный код

---
+++ id.c
@@ -0,0 +1,18 @@
+//подключение библиотек
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+//главная ф-ия программы 
+int main() {
+	uid_t userid;
+	gid_t groupid;
+//системный вызов getuid()
+	userid = getuid();
+//системный вызов getgid()
+	groupid = getgid();
+	printf("ид. пользователь: %d\n", userid);
+	printf("ид. группы пользователя: %d\n", groupid);
+//возврат значения "0' из главной ф-ии
+	return 0;
+}
