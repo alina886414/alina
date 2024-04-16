78ffac8530665140defb9f4cc6301bfcd928deba
A.S. Pichugina <flow1515@mail.ru>
2024-04-16 13:43:16 +0700

    создали исходный код для открытия на запись файла file1.txt

--- context1.c
+++ context1.c
@@ -1,5 +1,3 @@
-
-
 //родключение библиотек
 
 #include <sys/types.h>
@@ -8,11 +6,32 @@
 #include <unistd.h>
 // главная функция программы 
 int main(int argc, char *argv[], char *envp[]) {
-	int result;
-	//запуск программы cat с аргументами  "fork1.c"
-	result = execle("/bin/cat","/bin/cat", "fork1.c", 0, envp);
-	//printf() выполняется в случае ошибки
-	printf("ошибка при выполнении системного вызова execle()\n");
-	exit(-1);
-	return 0; //никогда не выполняется
+	int result1;
+	// объявление переменных 
+	pid_t procid,parentid; // инд.процесса
+	pid_t result; // результат системного вызова fork()
+	int a=0; // переменная для проверки  процесса родителя и процесаа ребенка
+	result1 = fork();
+	if (result < 0) {
+		printf("ошибка при выполнении fork()\n");
+		exit(-1);
+	}
+	else if (result == 0) {
+		a = a+1;
+		procid = getpid(); // получение инд. текущего процесса
+		parentid = getppid(); // получение инд. родительского процесса 
+		printf("инд.процесса: %d, инд. родителя: %d, значение переменной a: %d\n", procid, parentid, a);
+		result1 = execle("/bin/cat","/bin/cat", "fork1.c", 0, envp);
+       		printf("ошибка при выполнении системного вызова execle()\n");
+       		exit(-1);
+	
+	}
+	else {
+		a = a + 555;	
+        	procid = getpid(); // получение инд. текущего процесса
+    	        parentid = getppid(); // получение инд. родительского процесса 
+	        printf("инд.процесса: %d, инд. родителя: %d, значение переменной a: %d\n", procid, parentid, a);
+
+        }
+	return 0;
 }
