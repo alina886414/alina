b2f082dcb70275560cdeae2f2052a9e1e055a774
A.S. Pichugina <flow1515@mail.ru>
2024-05-21 13:10:33 +0700

    добавили обработку ошибочного ввода

--- Help.java
+++ Help.java
@@ -2,33 +2,38 @@ class Help {
 	public static void main (String args[]) 
 		throws java.io.IOException {
 		
-		char choice;
+		char choice,ignore;
 
-		System.out.println("Справочная система");
-		System.out.println(" 1. if");
-		System.out.println(" 2. switch");
-		System.out.println("выберите подраздел: ");
-		choice = (char) System.in.read();
+		do {
+			System.out.println("Справочная система");
+			System.out.println(" 1. if");
+			System.out.println(" 2. switch");
+			System.out.println("выберите подраздел: ");
+			choice = (char) System.in.read();
+				do {
+					ignore = (char) System.in.read();
+				}while (ignore != '\n');
+			} while (choice < '1' | choice > '2');
 
-		System.out.println("\n");
+			System.out.println("\n");
 	
-		switch (choice) {
-			case '1':
-				System.out.println("Operator if:\n");
-				System.out.println("if(условие)операторы; ");
-				System.out.println("else операторы");
-				break;
-			case '2':
-				System.out.println("Operator switch:\n");
-                                System.out.println("switch(условие) { ");
-                                System.out.println("	case constanta");
-				System.out.println("	последовательность операторов");
-                                System.out.println("	break; ");
-                                System.out.println("  ...");
-                                System.out.println(" } ");
-				break;
-			default:
-				System.out.print("неверный номер подраздела");
+			switch (choice) {
+				case '1':
+					System.out.println("Operator if:\n");
+					System.out.println("if(условие)операторы; ");
+					System.out.println("else операторы");
+					break;
+				case '2':
+					System.out.println("Operator switch:\n");
+                                	System.out.println("switch(условие) { ");
+                                	System.out.println("	case constanta");
+					System.out.println("	последовательность операторов");
+                                	System.out.println("	break; ");
+                                	System.out.println("  ...");
+                                	System.out.println(" } ");
+					break;
+				default:
+					System.out.print("неверный номер подраздела");
 		}
 
 	} 
