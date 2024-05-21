a198a661aa633dee6b3a1af28243c211bc7bf296
A.S. Pichugina <flow1515@mail.ru>
2024-05-21 13:37:17 +0700

    добавили бесконечный цикл в работу программы и выход по клавише

--- Help.java
+++ Help.java
@@ -3,6 +3,7 @@ class Help {
 		throws java.io.IOException {
 		
 		char choice,ignore;
+		for(;;) {
 
 		do {
 			System.out.println("Справочная система");
@@ -11,13 +12,16 @@ class Help {
 			System.out.println(" 3. for");
                         System.out.println(" 4. while");
 			System.out.println(" 5. do-while");
+			System.out.println("q - выход из программы");
 
 			System.out.println("выберите подраздел: ");
 			choice = (char) System.in.read();
 				do {
 					ignore = (char) System.in.read();
 				}while (ignore != '\n');
-			} while (choice < '1' | choice > '2');
+			} while (choice < '1' | choice > '5' & choice != 'q');
+			
+			if(choice == 'q') break;
 
 			System.out.println("\n");
 	
@@ -43,7 +47,7 @@ class Help {
 					break;
 				case '4':
 					System.out.println("цикл whule:\n ");
-                                        System.out.println("while(условие) операторы; ")
+                                        System.out.println("while(условие) операторы; ");
 					break;
 				case '5':
 					System.out.println("цикл do-while:\n");
@@ -51,7 +55,9 @@ class Help {
                                         System.out.println("операторы; ");
 					System.out.println("} while(условие); ");
                                  	break;
-		}
+			}
+			System.out.println();
+			}
 
 	} 
 
