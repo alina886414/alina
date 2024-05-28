56ea174d22106f18b39978da05bef25d482361bf
alina886414 <165747954+alina886414@users.noreply.github.com>
2024-05-28 10:46:16 +0700

    Update Help3.java
    ДОБАВИЛИ 2 КЕЙСА

--- Help3.java
+++ Help3.java
@@ -4,21 +4,20 @@ class Help {
 		
 		char choice,ignore;
 		
-    for(;;) {
+    		for(;;) {
 
 		do {
 			System.out.println("Справочная система");
 			System.out.println(" 1. if");
 			System.out.println(" 2. switch");
 			System.out.println(" 3. for");
-      System.out.println(" 4. while");
+      			System.out.println(" 4. while");
 			System.out.println(" 5. do-while");
-      System.out.println(" 6. break");
-      System.out.println(" 7. continue\n");
+      			System.out.println(" 6. break");
+      			System.out.println(" 7. continue\n");
 			System.out.println("q - выход из программы");
-      
-
-			System.out.println("выберите подраздел: ");
+      			System.out.println("выберите подраздел: ");
+			
 			choice = (char) System.in.read();
 				do {
 					ignore = (char) System.in.read();
@@ -31,44 +30,44 @@ class Help {
 	
 			switch (choice) {
 				case '1':
-					    System.out.println("Operator if:\n");
-					    System.out.println("if(условие)операторы; ");
+					System.out.println("Operator if:\n");
+					System.out.println("if(условие)операторы; ");
 				    	System.out.println("else операторы");
 				    	break;
 				case '2':
-					    System.out.println("Operator switch:\n");
-              System.out.println("switch(условие) { ");
-              System.out.println("	case constanta");
-			      	System.out.println("	последовательность операторов");
-              System.out.println("	break; ");
-              System.out.println("  ...");
-              System.out.println(" } ");
-					    break;
+					System.out.println("Operator switch:\n");
+        				System.out.println("switch(условие) { ");
+              				System.out.println("	case constanta");
+			      		System.out.println("	последовательность операторов");
+              				System.out.println("	break; ");
+              				System.out.println("  ...");
+              				System.out.println(" } ");
+					break;
 				case '3':
-					  System.out.println("Цикл for:\n ");
-            System.out.println("for(инициализация; условие; интерация");
-            System.out.println("операторы");
-					  break;
+					System.out.println("Цикл for:\n ");
+            				System.out.println("for(инициализация; условие; интерация");
+            				System.out.println("операторы");
+					break;
 				case '4':
 					System.out.println("цикл whule:\n ");
-          System.out.println("while(условие) операторы; ");
+         				System.out.println("while(условие) операторы; ");
 					break;
 				case '5':
 					System.out.println("цикл do-while:\n");
-          System.out.println("do {");
-          System.out.println("операторы; ");
+          				System.out.println("do {");
+          				System.out.println("операторы; ");
 					System.out.println("} while(условие); ");
-          break;
-        case '6':
+          				break;
+        			case '6':
 					System.out.println("Инструкция break:\n ");
-          System.out.println("break; или break метка; ");
-          break;
-        case '7':
+          				System.out.println("break; или break метка; ");
+          				break;
+        			case '7':
 					System.out.println("инструкция continue:\n ");
-          System.out.println("continue; или continue метка; ");
+          				System.out.println("continue; или continue метка; ");
 					break;
 			}
-      System.out.println();
+      		System.out.println();
       }
    }		
 }
